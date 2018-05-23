b1f1c73c044d20b69a36597190c539492d88d1ec
Fabian Vogelsteller <fabian@frozeman.de>
2018-05-23 11:46:31 +0200

    publishded new version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'frozeman:persistent-minimongo2',
   summary: 'Persistent Client-side Collections for Meteor using indexedDB, webSQL or localstorage',
-  version: '0.3.5',
+  version: '0.4.0',
   git: 'http://github.com/frozeman/meteor-persistent-minimongo'
 });
 
